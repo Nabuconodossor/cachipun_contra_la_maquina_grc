dd64dba68b23916bd3da3e34a5eb020e144299a9
Gabriel Rodríguez <96874381+Nabuconodossor@users.noreply.github.com>
2022-01-31 20:21:06 -0300

    Update app.js

--- app.js
+++ app.js
@@ -11,10 +11,93 @@
 // }
 
 
-let edad = parseInt(prompt("ingrese su edad: "));
+// let edad = parseInt(prompt("ingrese su edad: "));
 
-if (edad >= 18){
-    console.log("si puede ingresar");
-} else {
-    console.log("no puede ingresar");
+// if (edad >= 18){
+//     console.log("si puede ingresar");
+// } else {
+//     console.log("no puede ingresar");
+// }
+
+
+// let menu = prompt (`
+//     elija una opcion (ingrese un numero de la opcion)
+//     1. libros
+//     2. peliculas
+//     3.juegos
+
+// `);
+
+//     switch  (menu) {
+//         case    "1" : 
+//             console.log("principito");
+//             break;
+//         case    "2" :
+//             console.log("matrix");
+//             break;
+//         case    "3" :
+//             console.log("megamanx");
+//         default:
+//             console.log("ingreso cualquier tontera");
+//             break;
+//     }
+
+// let dia = prompt (" escriba un numero del 1 al 7"); 
+
+
+// switch(dia) {
+//     case "1":
+//         alert("El dia de la semana es lunes");
+//         break;
+//     case "2":
+//         alert("El dia de la semana es martes");
+//         break;
+//     case "3":
+//         alert("El dia de la semana es miercoles");
+//         break;
+//     case "4":
+//         alert("El dia de la semana es jueves");
+//         break;
+//     case "5":
+//         alert("El dia de la semana es viernes");
+//         break;
+//     case "6":
+//         alert("El dia de la semana es sabado");
+//         break;
+//     case "7":
+//         alert("El dia de la semana es domingo");
+//         break;
+//     default:
+//         alert("no escriba lo que usted quiera ingrese un numero")
+//         break;
+// }
+
+// let numero = 0 
+
+// while(numero<= 20) {
+//     console.log(numero);
+
+//     numero = numero + 2;
+// }
+
+// console.log("fin numero: " + numero );
+                                        
+
+                                            //pregunta espacio
+// let numRamdon   = math.floor(Math.random ( )* ( 11 - 1)) + 1 ;
+// console.log(numeroMaquina);
+
+// let numeroUsuario = parseInt(prompt("adivine un numero del 1 al 10"))
+
+// let vida = 3
+
+// while(numeroMaquina !== numeroUsuario && vidas > 1){
+//     numeroUsuario=  parseInt(prompt("tienes otro intento, vidas restantes: " + vidas))
+
+//     vidas --
+// }
+
+for (let i = 0; i < 10; i++) {
+    console.log(i);
 }
+
