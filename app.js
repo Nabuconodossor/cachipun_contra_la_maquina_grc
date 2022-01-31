89973013b3e2bf14ebfce6d5e396ead03c172074
Nabuconodossor <gaferoco@gmail.com>
2022-01-31 06:26:31 -0300

    cachipuunk

---
+++ app.js
@@ -0,0 +1,20 @@
+
+// let resultado = prompt("escribe si o no ")
+// console.log(resultado);
+
+// if(resultado.toLowerCase() === "si" )   {
+//     console.log("escribio si")
+// }   else if(resultado.toLowerCase () === "no") {
+//     console.log("escribio no")
+// }   else    {
+//     console.log("escribio cualquier payasada")
+// }
+
+
+let edad = parseInt(prompt("ingrese su edad: "));
+
+if (edad >= 18){
+    console.log("si puede ingresar");
+} else {
+    console.log("no puede ingresar");
+}
